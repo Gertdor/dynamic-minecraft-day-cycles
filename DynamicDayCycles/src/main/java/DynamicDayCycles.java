e8f9842a01b41e7e4fbb4e224b5398a3959e3d4f
Sebastian Norlin <wazazew@gmail.com>
2016-01-22 12:28:58 +0100

    created plugin skeleton

---
+++ DynamicDayCycles/src/main/java/DynamicDayCycles.java
@@ -0,0 +1,15 @@
+/**
+ * Created by basse on 2016-01-22.
+ */
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DynamicDayCycles extends JavaPlugin{
+    @Override
+    public void onEnable(){
+
+    }
+    @Override
+    public void onDisable(){
+        
+    }
+}
